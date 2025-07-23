b576561eb57ca1f55b39616d69d86bda44d2c1a9
Angelitoo777 <angeloropezaz1604@gmail.com>
2025-07-23 13:18:37 -0400

    Se creo todo el register y login para los usuarios con autenticacion de jwt.

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../database/mysql.js'
+
+export const User = sequelize.define('users', {
+
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
