d5fd43f20f6face4db571d26cb44b899b3b9483e
Angelitoo777 <angeloropeza1604@gmail.com>
2025-07-24 13:14:25 -0400

    Update post.routes.js
    se añadio el middleware a patch y delete

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -9,5 +9,5 @@ routesPost.get('/post/popular', PostController.getPopularPosts)
 routesPost.get('/post/:id', PostController.getById)
 
 routesPost.post('/post', jwtMiddleware, PostController.createPost)
-routesPost.patch('/post/:id', PostController.updatePost)
-routesPost.delete('/post/:id', PostController.deletePost)
+routesPost.patch('/post/:id', jwtMiddleware ,PostController.updatePost)
+routesPost.delete('/post/:id', jwtMiddleware, PostController.deletePost)
